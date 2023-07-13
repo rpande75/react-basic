e65ded7eda4f699abfe850a800dd62a6ba2ce071
rpande75 <rbpande@gmail.com>
2023-07-13 16:12:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createBrowserHistory } from 'history';
 
-const history = createBrowserHistory({ basename: '/webapp' })
 ReactDOM.render(<App />, document.getElementById('root'));
